fbfbc43f96a723fa18b46204b3ad291b18eb2e4d
ccpjBoss <jpccastilho1999@gmail.com>
2022-12-01 18:15:31 +0000

    First day of advent of code

---
+++ 2022/day1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    println!("Welcome to Day 1 of advent of code!");
+
+    let file_name = "input/input.txt";
+
+    let mut current_sum = 0;
+    let mut elfs: Vec<i32> = Vec::new();
+
+    let file = fs::File::open(file_name).expect("Cannot open file");
+
+    let file = BufReader::new(file);
+    for line in file.lines().filter_map(|result| result.ok()) {
+        match line.parse::<i32>() {
+            Ok(number) => {
+                current_sum += number;
+            }
+            Err(_) => {
+                elfs.push(current_sum);
+                current_sum = 0;
+            }
+        }
+    }
+    elfs.push(current_sum);
+    elfs.sort();
+    let mut reverse = elfs.iter().rev();
+
+    println!(
+        "Total calories from top 3 elfs is: {}",
+        reverse.next().unwrap() + reverse.next().unwrap() + reverse.next().unwrap()
+    )
+}
